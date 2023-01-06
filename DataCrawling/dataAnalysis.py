a7ece3d26fa5a35080f682d3c78d57ccb349718f
Hajong Kim <whoami125@g.skku.edu>
2023-01-06 18:30:11 +0900

    Updated: check if there is repeated lotto num & draft version of lotto predict

--- DataCrawling/dataAnalysis.py
+++ DataCrawling/dataAnalysis.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 from collections import Counter
-import os, json, sys
+import os, json, sys, random
 
 
 No1 = [] 
@@ -65,7 +65,7 @@ def LottoResultAllTimeDownload():
             "Num6": No6, 
             "bnsNum": bonusNo}, 
         "datasetNum" : len(resultCSV)
-        }
+    }
 
     script_dir = os.path.dirname(__file__)
     #print(frequencyData, script_dir)
@@ -114,7 +114,52 @@ def createPieChart(input):
     #이미지 저장
     plt.savefig('./DataCollection/{input}.png'.format(input = input), dpi=200, transparent=True)
     plt.close()
-        
+
+#반복된 정보 알아내기, 예를 들어서 하나의 로또에 같은 숫자가 2번 있는지, 같은 로또 번호가 당첨된적 있는지
+def checkifrepeated():
+    resultCSV = pd.read_csv("./DataCollection/lottoResultAllTime.csv")
+
+    u, c = np.unique(resultCSV, axis=0, return_counts=True)
+    print((c>1).any())
+
+# 데이터 추출 필요 정보 기능들
+# 랜덤 추천 로또 번호 생성 (최대 5개) 그리고 최근 100개랑 비교해서 적중률 테스트
+# 데이터 범위 설정 기능
+# 숫자별 가장 빈도 높은 로또들 번호들 사이에서 뽑기 (n개 사용자 지정, 비율 조정/x)
+# 숫자별 가장 빈도 낮은 로또들 번호들 사이에서 뽑기 (n개 사용자 지정, 비율 조정/x)
+# 최근 데이터 n개 기준
+# 내가 원하는 숫자 입력하고 비교
+
+
+#https://www.machinelearningplus.com/pandas/pandas-duplicated/
+#https://stackoverflow.com/questions/70506590/i-want-to-check-if-there-are-any-identical-rows-in-a-matrix
+#https://www.google.com/search?q=pandas+numpy+check+if+there+is+repeated+row+and+return+value&newwindow=1&sxsrf=ALiCzsaS-gqhtNmV2FFlP38se4lWiKbDTQ%3A1672939431480&ei=pwe3Y9n5HJiW-Aa9vqWQDQ&oq=pandas+numpy+check+if+there+is+repeated+row+and+return&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAxgAMgUIIRCgATIFCCEQoAEyBQghEKABOgoIABBHENYEELADOgQIIRAVOgcIIRCgARAKSgQIQRgASgQIRhgAULDGA1i00gNg_dsDaAJwAXgAgAH1AYgB4A2SAQUwLjkuMpgBAKABAcgBCMABAQ&sclient=gws-wiz-serp
+def lottopredict(amount):
+    returnLottoArray = []
+    lottoNumPredict = []
+    sampleSize = 7
+    lottoPlaceCount = 0
+
+
+    for i in range(int(amount)):
+
+        while lottoPlaceCount < sampleSize:
+            r = random.randint(0,45)
+            if r not in lottoNumPredict:
+                lottoPlaceCount += 1
+                lottoNumPredict.append(r)
+
+        returnLottoArray.append(lottoNumPredict)
+        lottoNumPredict = []
+        lottoPlaceCount = 0
+
+    returnLotto = {
+        "lottoPredict" : returnLottoArray,
+        "amount" : amount
+    }
+
+    print(returnLotto)
+
 
 
 if __name__ == '__main__':
@@ -124,5 +169,9 @@ if __name__ == '__main__':
         createLottoResultAllTime()
     elif sys.argv[1] == "LottoResultAllTimeDownload":
         LottoResultAllTimeDownload()
+    elif sys.argv[1] == "checkifrepeated":
+        checkifrepeated()
     elif sys.argv[1] == "createPieChart":
         createPieChart(sys.argv[2])
+    elif sys.argv[1] == "lottopredict":
+        lottopredict(sys.argv[2])
